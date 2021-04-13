588db7948f4deac7ad53aff734012975f3f0fe56
Joseph Toups <josephtoups95@gmail.com>
2021-04-12 20:16:29 -0700

    get

--- server.js
+++ server.js
@@ -17,7 +17,27 @@ var data = {
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'home.html'))}
-    );
+);
+
+app.get('/reserve', (req, res) => {
+    res.sendFile(path.join(__dirname, 'reserve.html'))
+})
+
+app.get ('/tables', (req, res) => {
+    res.sendFile(path.join (__dirname, 'tables.html'))
+})
 
 app.get('/reserve', (req, res) => {res.sendFile(path.join(__dirname, 'reserve.html'))}
 );
+
+app.get('/tables', (req, res) => {res.sendFile(path.join(__dirname, 'tables.html'))}
+);
+
+app.get ('/api/reservations', (req, res) => {
+    res.json(data.reservations)
+});
+
+app.get ('/api/waitlist', (req, res) => {
+    res.json(data.waitlist)
+});
+
